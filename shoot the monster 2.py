fabb0dcbb2ce5eeedd1466153aa04da1d1c3571b
SigmaBoi2014 <bernardo2014mendes@gmail.com>
2025-03-05 07:35:57 -0800

    Add files via upload

--- shoot the monster 2.py
+++ shoot the monster 2.py
@@ -1,15 +1,19 @@
 import pgzrun
 import random
 
+
 WIDTH =500
 HEIGHT =500
+
 msg="shoot the monster"
 result=""
 goodshots=0
+
 badshots=0
-monster=Actor("cookie")
+monster=Actor("the cookie")
 monster.x=250
 monster.y=250
+
 def draw():
     screen.fill("black")
     monster.draw()
@@ -18,10 +22,24 @@ def draw():
     screen.draw.text("goodshots "+str(goodshots),(100,30))
     screen.draw.text("badshots "+str(badshots),(100,50))
 
+    if result=="bad shot":
+        screen.fill("red")
+        monster.draw()
+        screen.draw.text(msg,(250,30))
+        screen.draw.text(result,(250,50))
+        screen.draw.text("goodshots "+str(goodshots),(100,30))
+        screen.draw.text("badshots "+str(badshots),(100,50))
+    if result== "good shot":
+        screen.fill("green")
+        monster.draw()
+        screen.draw.text(msg,(250,30))
+        screen.draw.text(result,(250,50))
+        screen.draw.text("goodshots "+str(goodshots),(100,30))
+        screen.draw.text("badshots "+str(badshots),(100,50))
 def on_mouse_down(pos):
     global result
     global goodshots
-    global badshot
+    global badshots
     if monster.collidepoint(pos):
         monster.x=random.randint(50,450)
         monster.y=random.randint(50,450)
@@ -30,4 +48,5 @@ def on_mouse_down(pos):
     else:
         result="bad shot"
         badshots+=1
+        
 pgzrun.go()
